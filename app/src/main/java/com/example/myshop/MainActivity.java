fa67c3c72d2da35eebbb43d85b4adfe58be2f1df
saptarshi <36296989+Saptarshisamata@users.noreply.github.com>
2020-04-01 16:30:24 +0530

    Shop app

--- app/src/main/java/com/example/myshop/MainActivity.java
+++ app/src/main/java/com/example/myshop/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.myshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView itemList = findViewById(R.id.list);
+        View view = findViewById(R.id.emptyView);
+        itemList.setEmptyView(view);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.homemenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        int id = item.getItemId() ;
+
+        switch (id){
+            case R.id.dlt :
+                // delete all data
+                return true ;
+            default:
+                throw new IndexOutOfBoundsException("not valid item selected");
+        }
     }
 }
